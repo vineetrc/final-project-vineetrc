364419a22e728dd6c1632eeaf1515781bb97b1f0
vineetrc <vineetreddi@gmail.com>
2020-12-01 17:54:22 -0800

    Added a check for basic win conditions and handle a pocketed cue ball scenario

--- include/core/engine.h
+++ include/core/engine.h
@@ -28,27 +28,42 @@ class Engine {
 
   int GetWinCondition() const;
 
+  bool HasCueBallSunk() const; // returns true or false depending on if cue ball has been sunk
+
+  void AddCueBall(); // changes variables to let engine know that cue ball has been placed
+
  private:
 
-  bool player_one_turn_;
+  bool is_player_one_turn_; // true if it is player one's turn, false for player two
 
+  // update variable to make sure game engine calculates the new situation at the begining of each players turn
   bool has_changed_;
 
   bool player_two_turn_;
 
-  int player_one_score_; //starts at 0 and goes till 8 represents every ball pocketed
+  int player_one_score_; // starts at 0 and goes till 8 represents every ball pocketed
 
-  int player_two_score_;
+  int player_two_score_; // starts at 0 and goes till 8 represents every ball pocketed
 
-  int red_ball_player_; // represents what player (1 or 2) the ball color is assigned to, 0 if unassigned
+  int red_ball_player_; // represents what player (1 or 2) the red ball color is assigned to, 0 if unassigned
 
-  int blue_ball_player_;
+  int blue_ball_player_; // represents what player (1 or 2) the blue ball color is assigned to, 0 if unassigned
 
-  int player_one_win_; // returns 1 or 2 depending on which player won, 0 if there is no winner
+  int winner_; // returns 1 or 2 depending on which player won, 0 if there is no winner
 
+  bool cue_ball_sunk; // true if the cue ball has been sunk on a turn
+
+  // determines which ball color corresponds to each player based on first sunk ball
   void CalculatePlayerBalls(visualizer::Board &game_board);
 
+  // counts all the different types of pocketed balls in a run
   std::vector<size_t> CountBallTypes(visualizer::Board &game_board);
+
+  void UpdateScores(std::vector<size_t>& counts);
+
+  void CheckForWinner(std::vector<size_t>& counts);
+
+  void CheckForExtraTurn(std::vector<size_t>& counts);
 
 };
 
--- src/core/engine.cc
+++ src/core/engine.cc
@@ -6,13 +6,14 @@ namespace pool{
     player_one_score_ = 0;
     player_two_score_ = 0;
 
-    player_one_turn_ = false;
+    is_player_one_turn_ = false;
     player_two_turn_ = false;
 
     red_ball_player_ = 0;
     blue_ball_player_ = 0;
     has_changed_ = false;
-    player_one_win_ = 0; // corresponds to no winner
+    winner_ = 0; // corresponds to no winner
+    cue_ball_sunk = false;
   }
 
   void Engine::Update(visualizer::Board &game_board){
@@ -24,69 +25,29 @@ namespace pool{
     }
     std::vector<size_t> counts = CountBallTypes(game_board);
 
-    // determines if player one wins due to pocketing 8 ball
-    if(player_one_turn_){
-      if(counts[3] == 1){
-        if(player_one_score_ == 7){
-          player_one_win_ = 1;
-        }
-        else{
-          player_one_win_ = 2;
-        }
-      }
+    CheckForWinner(counts);
+
+    UpdateScores(counts);
+
+    if(counts[2] == 1){
+        // cue ball has been sunk
+        cue_ball_sunk = true;
     }
 
-    // determines if player two wins due to pocketing 8 ball
-    if(!player_one_turn_){
-      if(counts[3] == 1){
-        if(player_two_score_ == 7){
-            player_one_win_ = 2;
-        }
-        else{
-            player_one_win_ = 1;
-        }
-      }
-    }
+    CheckForExtraTurn(counts);
 
-    if(red_ball_player_ == 1) {
-      // player one has control of red balls
-      player_one_score_ += counts[0]; // adding red balls to player one
-      player_two_score_ +=counts[1]; // adding blue balls to player two
-    }
-    if(red_ball_player_ == 2) {
-      // player two has control of red balls
-      player_one_score_ += counts[1]; // adding blue balls to player one
-      player_two_score_ +=counts[0]; // adding red balls to player two
-    }
-
-
-    if(player_one_turn_){
-      // checking to see if a ball of the same color has been pocketed, then dont switch player 1 turn
-      if((red_ball_player_ == 1 && counts[0] > 0) || (red_ball_player_ == 2 && counts[1] > 0)){
-        player_one_turn_ = !player_one_turn_;
-      }
-    }
-
-    if(!player_one_turn_){
-      // checking to see if a ball of the same color has been pocketed, then dont switch player 2 turn
-      if((red_ball_player_ == 1 && counts[1] > 0) || (red_ball_player_ == 2 && counts[0] > 0)){
-          player_one_turn_ = !player_one_turn_;
-      }
-    }
-
-    player_one_turn_ = !player_one_turn_;
+    is_player_one_turn_ = !is_player_one_turn_; // flip player turn
     has_changed_ = true;
 
-      // clear out all the balls and analyze them
-      // analyze if ball match
-    game_board.ClearPocketedBalls();
+
+    game_board.ClearPocketedBalls(); // clear out all the balls after updating player states
     }
+
     if(game_board.GetTurnStatus()==false){
       // board is still running a turn
       has_changed_ = false;
     }
   }
-
 
 void Engine::CalculatePlayerBalls(visualizer::Board &game_board) {
   // assigns the player what ball they will have for the rest of the game, based on first sunk ball
@@ -102,7 +63,7 @@ void Engine::CalculatePlayerBalls(visualizer::Board &game_board) {
   }
   if(red_count + blue_count !=0) {
     if (red_count >= blue_count) {
-      if(player_one_turn_ == true) {
+      if(is_player_one_turn_ == true) {
         red_ball_player_ = 1;
         blue_ball_player_ = 2;
       }
@@ -112,7 +73,7 @@ void Engine::CalculatePlayerBalls(visualizer::Board &game_board) {
       }
     }
     if (red_count < blue_count) {
-      if(player_one_turn_ == true) {
+      if(is_player_one_turn_ == true) {
         red_ball_player_ = 2;
         blue_ball_player_ = 1;
       }
@@ -133,7 +94,7 @@ int Engine::GetPlayerTwoScore() const {
 }
 
 bool Engine::GetPlayerOneTurn() const {
-    return player_one_turn_;
+    return is_player_one_turn_;
 }
 
 int Engine::GetRedBallPlayer() const {
@@ -165,8 +126,78 @@ ci::Color Engine::GetPlayerTwoColor() const {
 }
 
 int Engine::GetWinCondition() const {
-    return player_one_win_;
+    return winner_;
 }
+
+bool Engine::HasCueBallSunk() const {
+    return cue_ball_sunk;
+}
+
+void Engine::AddCueBall() {
+    cue_ball_sunk = false;
+}
+
+void Engine::UpdateScores(std::vector<size_t> &counts) {
+  if(red_ball_player_ == 1) {
+    // player one has control of red balls
+    player_one_score_ += counts[0]; // adding red balls to player one
+    player_two_score_ +=counts[1]; // adding blue balls to player two
+  }
+  if(red_ball_player_ == 2) {
+    // player two has control of red balls
+    player_one_score_ += counts[1]; // adding blue balls to player one
+    player_two_score_ +=counts[0]; // adding red balls to player two
+  }
+}
+
+void Engine::CheckForWinner(std::vector<size_t> &counts) {
+  // determines if player one wins due to pocketing 8 ball
+  if(is_player_one_turn_){
+    if(counts[3] == 1){
+      if(player_one_score_ == 7){
+        player_one_score_++;
+        winner_ = 1;
+      }
+      else{
+        winner_ = 2;
+      }
+    }
+  }
+
+  // determines if player two wins due to pocketing 8 ball
+  if(!is_player_one_turn_){
+    if(counts[3] == 1){
+      if(player_two_score_ == 7){
+        player_two_score_++;
+        winner_ = 2;
+      }
+      else{
+        winner_ = 1;
+      }
+    }
+  }
+}
+
+void Engine::CheckForExtraTurn(std::vector<size_t> &counts) {
+  if(cue_ball_sunk == false) {
+    //if no cue ball foul has occured, the player's turn may stay the same may be switched
+    if (is_player_one_turn_) {
+      // checking to see if a ball of the same color has been pocketed, then dont switch player 1 turn
+      if ((red_ball_player_ == 1 && counts[0] > 0) || (red_ball_player_ == 2 && counts[1] > 0)) {
+        is_player_one_turn_ = !is_player_one_turn_; // keep the same player's turn
+      }
+    }
+
+    if (!is_player_one_turn_) {
+      // checking to see if a ball of the same color has been pocketed, then dont switch player 2 turn
+      if ((red_ball_player_ == 1 && counts[1] > 0) || (red_ball_player_ == 2 && counts[0] > 0)) {
+        is_player_one_turn_ = !is_player_one_turn_; // keep the same player's turn
+      }
+    }
+  }
+
+}
+
 
 std::vector<size_t> Engine::CountBallTypes(visualizer::Board &game_board) {
   size_t red_count = 0;
