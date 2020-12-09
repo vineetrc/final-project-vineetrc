90bdc3133cc67efc9af014b81a399960a67e2bbc
vineetrc <vineetreddi@gmail.com>
2020-12-08 19:14:29 -0800

    Added some const variables. Organized Code

--- src/visualizer/pool_app.cc
+++ src/visualizer/pool_app.cc
@@ -1,49 +1,27 @@
 #include <visualizer/pool_app.h>
 
 namespace pool {
-
 namespace visualizer {
 
 PoolApp::PoolApp()
     : game_board_(glm::vec2(300, 200), 800 , 350),
     game_engine_(){
 
-  ci::app::setWindowSize(1400, 800);
+  ci::app::setWindowSize(window_size_x_, window_size_y_);
   update_speed_ = 1; //number of simulation 'steps' gone thru on one update call
-  isStartScreen = true;
+  is_start_screen_ = true;
 }
 
 void PoolApp::draw() {
   ci::Color8u background_color(255, 246, 148);  // light yellow
   ci::gl::clear(background_color);
 
-
-  if(!isStartScreen) {
+  if (!is_start_screen_) {
+    // displays game screen
     std::string player_turn = "Player 1";
-    if (game_engine_.GetPlayerOneTurn() == false) {
+    if (!game_engine_.GetPlayerOneTurn()) {
       player_turn = "Player 2";
     }
-
-//  std::string game_message = "Play in progress";
-//  if(game_board_.GetTurnStatus()==true){
-//    game_message = "hit the cue ball";
-//  }
-//
-//  if(game_engine_.HasIncorrectColorHit()==true) {
-//    game_message = "Foul. Previous Player Didn't Hit Their Own Ball Color. Current Player hit the cue ball";
-//  }
-//  if(game_engine_.HasCueBallSunk()==true) {
-//    game_message = "click to place the cue ball";
-//  }
-//
-//  if(game_engine_.GetWinCondition()== 1){
-//    game_message = "player one Wins";
-//  }
-//
-//  if(game_engine_.GetWinCondition()== 2){
-//    game_message = "player two Wins";
-//  }
-
 
     // displays message of Winning condition, Player turn, or to place the cue ball due to a foul
     ci::gl::drawStringCentered(game_engine_.GetGameMessage(game_board_),
@@ -51,14 +29,16 @@ void PoolApp::draw() {
                                ci::Color("black"));
 
     // displays which player turn it is
-
     if (game_engine_.GetWinCondition() == 0) {
-      ci::gl::drawStringCentered(player_turn + " turn", glm::vec2(getWindowWidth() * .5, 80), ci::Color("black"));
+      ci::gl::drawStringCentered(player_turn + " turn",
+                                 glm::vec2(getWindowWidth() * .5, 80), ci::Color("black"));
     }
 
     // drawing signs for the player ball racks
-    ci::gl::drawStringCentered("player 1", glm::vec2(getWindowWidth() * .075, 120), ci::Color("black"));
-    ci::gl::drawStringCentered("player 2", glm::vec2(getWindowWidth() * .925, 120), ci::Color("black"));
+    ci::gl::drawStringCentered("Player 1",
+                               glm::vec2(getWindowWidth() * .075, 120), ci::Color("black"));
+    ci::gl::drawStringCentered("Player 2",
+                               glm::vec2(getWindowWidth() * .925, 120), ci::Color("black"));
 
     DrawGameExternalities();  // draws player ball racks and power bar
 
@@ -70,12 +50,16 @@ void PoolApp::draw() {
   }
 }
 void PoolApp::mouseDrag(ci::app::MouseEvent event) {
-  end_coords_ = event.getPos();
-  CalculateForce();
+  if (!is_start_screen_) {
+    // record mouseDrag when on game screen
+    end_coords_ = event.getPos();
+    CalculateForce();
+  }
 }
 
 void PoolApp::update() {
-  if(game_engine_.GetWinCondition()==0) {
+  if (game_engine_.GetWinCondition()==0) {
+    // while no winner update game engine and board
     for (size_t i = 0; i < update_speed_; i++) {
       game_board_.Update();
     }
@@ -84,53 +68,63 @@ void PoolApp::update() {
 }
 
 void PoolApp::mouseUp(ci::app::MouseEvent event) {
-  if(game_engine_.HasCueBallSunk() == true){
-    // allow current player to add the cue ball back to the board
-    if(game_board_.AddCueBall(event.getPos())) {
-      game_engine_.AddCueBall(); // resets state to no cue ball sunk
+  if (!is_start_screen_) {
+    // When on game screen
+    if (game_engine_.HasCueBallSunk()) {
+      // allow current player to add the cue ball back to the board
+      if (game_board_.AddCueBall(event.getPos())) {
+        game_engine_.AddCueBall(); // resets state to no cue ball sunk
+      }
+    } else if (game_engine_.GetWinCondition() == 0) {
+      end_coords_ = event.getPos();
+      CalculateForce();
+      game_board_.HandleClick(game_board_.GetMouseCoords(), force_);
+      force_ = 0;
     }
-  }
-  else if(game_engine_.GetWinCondition()==0){
-    end_coords_ = event.getPos();
-    CalculateForce();
-    game_board_.HandleClick(game_board_.GetMouseCoords(), force_);
-    force_ = 0;
   }
 }
 
 void PoolApp::mouseDown(ci::app::MouseEvent event) {
-  //adds particle to simulation
-  if(game_engine_.GetWinCondition()==0) {
-    // if no winner
-    init_coords_ = event.getPos();
+  if (!is_start_screen_) {
+    // when on game screen
+    //adds particle to simulation
+    if (game_engine_.GetWinCondition() == 0) {
+      // if no winner
+      init_coords_ = event.getPos();
+    }
   }
 }
 
 void PoolApp::mouseMove(ci::app::MouseEvent event) {
   //adds particle to simulation
-  if(game_engine_.GetWinCondition()==0) {
-    // if no winner
-    game_board_.UpdateMousePosition(event.getPos());
+  if (!is_start_screen_) {
+    //when on game screen
+    if (game_engine_.GetWinCondition() == 0) {
+      // if no winner, update user mouse position over board
+      game_board_.UpdateMousePosition(event.getPos());
+    }
   }
 }
 
 void PoolApp::keyDown(ci::app::KeyEvent event) {
   switch (event.getCode()) {
     case ci::app::KeyEvent::KEY_SPACE: {
-      isStartScreen = false;
+      is_start_screen_ = false; // go to game screen
       break;
     }
+
     case ci::app::KeyEvent::KEY_DELETE: {
       game_board_.Clear();
       break;
     }
 
     case ci::app::KeyEvent::KEY_UP: {
-      update_speed_++;
+      update_speed_++; // increases speed of game-play
       break;
     }
 
     case ci::app::KeyEvent::KEY_DOWN: {
+      // decreases speed of game-play
       if (update_speed_ <= 1) {
         break;
       }
@@ -183,21 +177,23 @@ void PoolApp::DrawStartScreen() {
 
 void PoolApp::DrawGameExternalities() {
   // drawing ball racks for players
-  //double const rack_x_pos = getWindowHeight()*.15;
 
+  // draw ball rack for player 1
   ci::gl::color(ci::Color("grey"));
   ci::Rectf player_one_rack(getWindowWidth()*.05, getWindowHeight()*.3, getWindowWidth()*.08,
                             getWindowHeight()*.7 );
   ci::gl::drawSolidRoundedRect(player_one_rack, 20);
 
+  // draw ball rack for player 2
   ci::Rectf player_two_rack(getWindowWidth()*.92, getWindowHeight()*.3, getWindowWidth()*.95,
                             getWindowHeight()*.7 );
   ci::gl::drawSolidRoundedRect(player_two_rack, 20);
 
   //displaying balls remaining for player one
-  ci::gl::color(ci::Color("black"));
-  for(int i = 0; i < 8 - game_engine_.GetPlayerOneScore(); i++){
+  ci::gl::color(ci::Color("black")); // color of the first ball
+  for(int i = 0; i < game_engine_.GetWinningScore() - game_engine_.GetPlayerOneScore(); i++){
     if(game_engine_.GetWinCondition() != 1) {
+      // displays only if player 1 hasn't won
       ci::gl::drawSolidCircle(glm::vec2(getWindowWidth() * .065,
                                         getWindowHeight() * .3 + 20 + i * 40), 20);
       ci::gl::color(game_engine_.GetPlayerOneColor());
@@ -205,9 +201,10 @@ void PoolApp::DrawGameExternalities() {
   }
 
   // displaying balls remaining for player two
-  ci::gl::color(ci::Color("black"));
-  for(int i = 0; i < 8 - game_engine_.GetPlayerTwoScore(); i++){
+  ci::gl::color(ci::Color("black")); // color of the first ball on the rack
+  for(int i = 0; i < game_engine_.GetWinningScore() - game_engine_.GetPlayerTwoScore(); i++){
     if(game_engine_.GetWinCondition() != 2) {
+      // displays only if player 2 hasn't won
       ci::gl::drawSolidCircle(glm::vec2(getWindowWidth() * .935,
                                         getWindowHeight() * .3 + 20 + i * 40), 20);
       ci::gl::color(game_engine_.GetPlayerTwoColor());
@@ -219,8 +216,9 @@ void PoolApp::DrawGameExternalities() {
   glm::vec2 top_left = ci::vec2(getWindowWidth()*.5-120,getWindowHeight()*.85);
   glm::vec2 bottom_right = ci::vec2(getWindowWidth()*.5+120,getWindowHeight()*.85+50);
   ci::Rectf full_power_bar(top_left, bottom_right);
-  ci::gl::drawSolidRoundedRect(full_power_bar, 20);
+  ci::gl::drawSolidRoundedRect(full_power_bar, 20); // drawing total shot bar
 
+  // drawing force bar of variable length
   ci::gl::color(ci::Color("red"));
   bottom_right.x = getWindowWidth()*.5 - 120 + 240*(force_/max_force_);
   ci::Rectf power_bar(top_left,bottom_right);
